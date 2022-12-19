6af46720e4de403d0a82bd64c3c1402fe9c26adb
efhiii <efhiii2000@gmail.com>
2022-12-18 17:13:18 -0800

    Drop file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ let txt = document.getElementById('txt');
 let errors = document.getElementById('errors');
 let result = document.getElementById('txt-result');
 let download = document.getElementById('download');
+let dropZone = document.getElementById('dropZone');
 
 function escapeHtml(unsafe) {
   return unsafe
@@ -31,25 +32,25 @@ function toGrouped(textCode) {
             txt[txt.length - 1][0].toUpperCase() +
             txt[txt.length - 1].slice(1);
           txt = txt.join(' ');
-        break;
+          break;
         case Case.LOWER_FIRST_CHAR:
           txt = txt.split(' ');
           txt[txt.length - 1] =
             txt[txt.length - 1][0].toLowerCase() +
             txt[txt.length - 1].slice(1);
           txt = txt.join(' ');
-        break;
+          break;
         case Case.LOWER:
           txt = txt.split(' ');
           txt[txt.length - 1] = txt[txt.length - 1].toLowerCase();
           txt = txt.join(' ');
-        break;
+          break;
         case Case.UPPER_FIRST_WORD:
         case Case.UPPER:
           txt = txt.split(' ');
           txt[txt.length - 1] = txt[txt.length - 1].toUpperCase();
           txt = txt.join(' ');
-        break;
+          break;
       }
     }
 
@@ -66,7 +67,7 @@ function toGrouped(textCode) {
       txt = (hasSpace ? ' ' : '') +
         parsed.retroCurrency.slice(0, indexOfC) +
         (hasPoint ? txt.split('.')[0] : txt).split('').reduceRight(
-          (a,b) => b + (a && a.length % 4 === 3 ? ',': '') + a, ''
+          (a, b) => b + (a && a.length % 4 === 3 ? ',' : '') + a, ''
         ) + (
           hasPoint ? '.' + txt.split('.')[1] +
           parsed.retroCurrency.slice(indexOfC + 1) : ''
@@ -98,8 +99,8 @@ function toStenoGrouped(txt) {
     let ln = lines[i].split(' ').slice(2);
     if(ln && ln.length > 0 && ln[0].indexOf('Translation(') === 0) {
       let stroke = ln.slice(0, ln.indexOf(':')).map(a => a.slice(1, a.length - 2)).join(' ')
-      .replace(/^ranslation\(\('/, '')
-      .replace(/'$/,'');
+        .replace(/^ranslation\(\('/, '')
+        .replace(/'$/, '');
       let txt = ln.slice(ln.indexOf(':') + 1).join(' ').replace(/^"/, '').replace(/"\)$/, '');
       ans.push([txt, stroke]);
       continue;
@@ -111,7 +112,7 @@ function toStenoGrouped(txt) {
   }
 
   if(ans.length === 0) {
-    errors.innerHTML = `<span class='error'>No translation logs found</span>`;
+    errors.innerHTML = `<span class='error'>No translation logs found <a href='https://github.com/EFHIII/LessonGen'>help</a></span>`;
     result.innerHTML = '';
     return;
   }
@@ -134,19 +135,21 @@ function toStenoGrouped(txt) {
     }
   }
 
+  ans = ans.map(a => a[0] === 'None)' ? [a[1], a[1]] : a);
+
   return toGrouped(ans);
 }
 
 let waiting = false;
 let waitingTimeout;
+
 function update(wait = false) {
   if(wait) {
     if(waiting) {
       waiting = Date.now();
       clearTimeout(waitingTimeout);
       waitingTimeout = setTimeout(update, 300);
-    }
-    else {
+    } else {
       waiting = Date.now();
       waitingTimeout = setTimeout(update, 300);
     }
@@ -162,14 +165,15 @@ function update(wait = false) {
     return;
   }
 
-  let groupedWords = [[...grouped[0]]];
+  let groupedWords = [
+    [...grouped[0]]
+  ];
   for(let i = 1; i < grouped.length; i++) {
     if(grouped[i][0][0] === ' ' ||
-    /^\s*([^\w\d]+ ?)+$/.test(grouped[i][0]) ||
-    /^\s*([^\w\d]+ ?)+$/.test(groupedWords[groupedWords.length - 1][0])) {
+      /^\s*([^\w\d]+ ?)+$/.test(grouped[i][0]) ||
+      /^\s*([^\w\d]+ ?)+$/.test(groupedWords[groupedWords.length - 1][0])) {
       groupedWords.push([...grouped[i]]);
-    }
-    else {
+    } else {
       groupedWords[groupedWords.length - 1][0] += '/' + grouped[i][0];
       groupedWords[groupedWords.length - 1][1] += '/' + grouped[i][1];
     }
@@ -185,21 +189,22 @@ function update(wait = false) {
   let longest = groupedWords.reduce((a, b) => Math.max(b[1].length, a), 0);
 
   errors.innerHTML = ``;
-  result.innerHTML = groupedWords.map(a => a[1].padEnd(longest, ' ') + '│' + a[0]+'\n').join('');
+  result.innerHTML = groupedWords.map(a => a[1].padEnd(longest, ' ') + '│' + a[0] + '\n').join('');
   download.style.display = 'inline';
 }
 
 function downloadLesson() {
   let grouped = toStenoGrouped(txt.value.trim());
 
-  let groupedWords = [[...grouped[0]]];
+  let groupedWords = [
+    [...grouped[0]]
+  ];
   for(let i = 1; i < grouped.length; i++) {
     if(grouped[i][0][0] === ' ' ||
-    /^\s*([^\w\d]+ ?)+$/.test(grouped[i][0]) ||
-    /^\s*([^\w\d]+ ?)+$/.test(groupedWords[groupedWords.length - 1][0])) {
+      /^\s*([^\w\d]+ ?)+$/.test(grouped[i][0]) ||
+      /^\s*([^\w\d]+ ?)+$/.test(groupedWords[groupedWords.length - 1][0])) {
       groupedWords.push([...grouped[i]]);
-    }
-    else {
+    } else {
       groupedWords[groupedWords.length - 1][0] += '/' + grouped[i][0];
       groupedWords[groupedWords.length - 1][1] += '/' + grouped[i][1];
     }
@@ -218,7 +223,7 @@ function downloadLesson() {
 
   groupedWords = groupedWords.map(a => a[0].trim() + '\n ' + (a[0][0] === ' ' ? '' : '/') + a[1]);
 
-  groupedWords[0] = groupedWords[0].replace('\n /','\n ');
+  groupedWords[0] = groupedWords[0].replace('\n /', '\n ');
 
   let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(groupedWords.join('\n'));
   let downloadAnchorNode = document.createElement('a');
@@ -229,8 +234,50 @@ function downloadLesson() {
   downloadAnchorNode.remove();
 }
 
+
+function parseFile(file) {
+  //read the file
+  const reader = new FileReader();
+  reader.onload = function(e) {
+    txt.value = e.target.result.split('\n').filter(a => {
+      return /^\*?Translation\(/.test(a.split(' ')[2]);
+    }).join('\n');
+    update();
+  }
+  reader.readAsText(file);
+}
+
+
 document.addEventListener('keydown', _ => update(true));
 
 download.addEventListener('click', _ => downloadLesson());
 
 document.getElementById('underlineBriefs').addEventListener('change', update);
+
+window.addEventListener('dragenter', e => {
+  dropZone.style.visibility = 'visible';
+});
+
+function allowDrag(e) {
+  e.dataTransfer.dropEffect = 'copy';
+  e.preventDefault();
+}
+
+function handleDrop(e) {
+  e.preventDefault();
+  dropZone.style.visibility = 'hidden';
+
+  parseFile(e.dataTransfer.files[0]);
+}
+
+dropZone.addEventListener('dragleave', e => {
+  if(e.fromElement === null) {
+    dropZone.style.visibility = 'hidden';
+  }
+});
+
+dropZone.addEventListener('dragenter', allowDrag);
+dropZone.addEventListener('dragover', allowDrag);
+
+
+dropZone.addEventListener('drop', handleDrop);
